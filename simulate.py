2b3083ae072759a48a1009a39ae5309cfe20f322
B.M <b.madadi@tudelft.nl>
2023-04-28 19:17:03 +0200

    final + fixed TO utilization issue

--- simulate.py
+++ simulate.py
@@ -20,15 +20,11 @@ def parameters():
     replication = 5
     sample_size = 0.01
     simulation_start = [0, 5, 8]
-    simulation_start = [5]
     simulation_duration = [9, 24]
-    simulation_duration = [9]
 
     ## model variation parameters
     to2v_ratios = np.array(list(range(30, 105, 5))) / 100
-    to2v_ratios = [0.3]
     takeover_times = [0, 1, 2, 3]
-    takeover_times = [3]
 
     ## operation parameters
     max_to_duration = 4.5 * 60
@@ -230,8 +226,8 @@ def run_simulation(replication_no, output_dir, runs, n_vh, n_to, setup_to, act_s
     utilization_vh_avg = np.sum(event_log[(event_log['Event'] =='Teleoperated') |
                                           (event_log['Event'] =='Takeover')]['Duration']) / (duration * n_vh)
     utilization_to_avg = np.sum(event_log[(event_log['Event'] =='Teleoperated') |
-                                              (event_log['Event'] =='Takeover') |
-                                              (event_log['Event'] =='Resting')]['Duration']) / (duration * n_to)
+                                          (event_log['Event'] =='Takeover') |
+                                          (event_log['Event'] =='Resting')]['Duration']) / (duration * n_to)
 
     # queues
     indices = states_vh_df.index.values
